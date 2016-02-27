cf4876a98a216e9b05c9e3843bab8cd05be52298
kalanda <kalanda@gmail.com>
2016-02-27 20:28:10 +0100

    Added npm-debug.log to cleaning section of install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "--- Clean any previous install"
-rm -rf node_modules dist .tmp .sass_cache
+rm -rf node_modules dist .tmp .sass_cache npm-debug.log
 npm cache clean
 
 echo "--- Install dependencies"
